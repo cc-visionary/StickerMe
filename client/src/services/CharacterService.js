be1049a62e3ef0e06068755ed9ca2d3d500dc532
ccvisionary <lim.christopher.go@gmail.com>
2021-07-30 09:33:34 +0800

    Completed the Character Service and Added Delete on Contact Service

--- client/src/services/CharacterService.js
+++ client/src/services/CharacterService.js
@@ -2,11 +2,18 @@
 
 import axios from "axios";
 
-const CHARACTER_API_BASE_URL = "http://localhost:3000/api/Characters";
+const CHARACTER_API_BASE_URL = "http://localhost:3000/api/characters";
 
 const CharacterService = {
   getAllCharactersByUserID: (userID) =>
     axios.get(CHARACTER_API_BASE_URL, { userID }),
+  getCharacter: (id) => axios.get(`${CHARACTER_API_BASE_URL}/id/${id}`),
+  addCharacter: (character) =>
+    axios.post(`${CHARACTER_API_BASE_URL}/add`, character),
+  editCharacter: (id, character) =>
+    axios.patch(`${CHARACTER_API_BASE_URL}/update`, { id, character }),
+  deleteCharacter: (id) =>
+    axios.delete(`${CHARACTER_API_BASE_URL}/delete`, { id }),
 };
 
 export default CharacterService;

19dfd939cafe92ca85009ff875a94f237299039c
ccvisionary <lim.christopher.go@gmail.com>
2021-08-02 10:23:43 +0800

    Finished the View for the Signup page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,7 +2,8 @@ import Admin from "./Admin";
 import Customer from "./Customer";
 import Landing from "./Landing";
 import Login from "./Login";
+import Signup from "./Signup";
 import Loading from "./Loading";
 import PageNotFound from "./PageNotFound";
 
-export { Admin, Customer, Landing, Login, Loading, PageNotFound };
+export { Admin, Customer, Landing, Login, Signup, Loading, PageNotFound };

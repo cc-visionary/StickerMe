764fcab0b09328c60d1f3cb7dc1ab56bc6ebf2d8
ccvisionary <lim.christopher.go@gmail.com>
2021-08-10 08:59:29 +0800

    Removed line break style

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
   },
   plugins: ['react'],
   rules: {
+    'linebreak-style': 0,
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'react/prefer-es6-class': [1, 'always'],
     'react/prefer-stateless-function': [1],

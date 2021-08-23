e84031a6097a5ad5457ac8c0ca96384ec8b5d1ae
Jakkushi <ian_angelo_racoma@dlsu.edu.ph>
2021-08-23 22:17:26 +0800

    Initial commit for prototype of customer-page

--- client/src/pages/Customer.jsx
+++ client/src/pages/Customer.jsx
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import { UserService } from '../services';
+import { getUser } from '../utils/store';
+
+import userInfoIcon from '../assets/images/icons/Profile.svg';
+import characterIcon from '../assets/images/icons/3 User.svg';
+import folderIcon from '../assets/images/icons/Folder.svg';
+import documentIcon from '../assets/images/icons/Document.svg';
+
+import '../assets/styles/pages/Customer.css';
 
 export default class Customer extends Component {
   constructor(props) {
@@ -18,14 +26,47 @@ export default class Customer extends Component {
   }
 
   render() {
+    const userInfo = getUser();
+    const userName = userInfo.uname;
+
     return (
       <div id="user-page">
-        <h1>Customer Page</h1>
-        <input
-          type="button"
-          onClick={() => this.handleLogout()}
-          value="Logout"
-        />
+        <div id="inner-user-page">
+          <h1 id="welcome_text">WELCOME</h1>
+          <p id="customer_name">{userName}</p>
+
+          <div id="feature_divider">
+            <p id="features_label">FEATURES</p>
+          </div>
+
+          <div id="customer_options">
+            <a className="option_button" href="/#">
+              <div className="option_div" id="user_info">
+                <img className="option_icon" src={userInfoIcon} alt="User Info" />
+                <p className="option_label">User Info</p>
+              </div>
+            </a>
+            <a className="option_button" href="/#">
+              <div className="option_div" id="characters">
+                <img className="option_icon" src={characterIcon} alt="Characters" />
+                <p className="option_label">Characters</p>
+              </div>
+            </a>
+            <a className="option_button" href="/#">
+              <div className="option_div" id="contacts">
+                <img className="option_icon" src={folderIcon} alt="User Info" />
+                <p className="option_label">Contacts</p>
+              </div>
+            </a>
+            <a className="option_button" href="/#">
+              <div className="option_div" id="order">
+                <img className="option_icon" src={documentIcon} alt="User Info" />
+                <p className="option_label">Order</p>
+              </div>
+            </a>
+          </div>
+
+        </div>
       </div>
     );
   }

91cc9cf21a503e75c5026039c04f2b292669b214
Jakkushi <ian_angelo_racoma@dlsu.edu.ph>
2021-08-24 23:25:35 +0800

    Made Initial Mobile Responsiveness layouts

--- client/src/pages/customer/CustomerHome.jsx
+++ client/src/pages/customer/CustomerHome.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { getUser } from '../../utils/store';
 
 import userInfoIcon from '../../assets/images/icons/Profile.svg';
@@ -8,56 +8,55 @@ import documentIcon from '../../assets/images/icons/Document.svg';
 
 import '../../assets/styles/pages/customer/CustomerHome.css';
 
-export default class CustomerHome extends Component {
-  constructor(props) {
-    super(props);
+const CustomerHome = () => {
+  const [hiddenImage1, setHiddenImage1] = useState(true);
+  const [hiddenImage2, setHiddenImage2] = useState(true);
+  const [hiddenImage3, setHiddenImage3] = useState(true);
+  const [hiddenImage4, setHiddenImage4] = useState(true);
 
-    this.state = {};
-  }
+  const userInfo = getUser();
+  const userName = userInfo.uname;
 
-  render() {
-    const userInfo = getUser();
-    const userName = userInfo.uname;
+  return (
+    <div id="user-page">
+      <div id="inner-user-page" hidden={hiddenImage1 || hiddenImage2 || hiddenImage3 || hiddenImage4}>
+        <h1 id="welcome_text">WELCOME</h1>
+        <p id="customer_name">{userName}</p>
 
-    return (
-      <div id="user-page">
-        <div id="inner-user-page">
-          <h1 id="welcome_text">WELCOME</h1>
-          <p id="customer_name">{userName}</p>
-
-          <div id="feature_divider">
-            <p id="features_label">FEATURES</p>
-          </div>
-
-          <div id="customer_options">
-            <a className="option_button" href="/#">
-              <div className="option_div" id="user_info">
-                <img className="option_icon" src={userInfoIcon} alt="User Info" />
-                <p className="option_label">User Info</p>
-              </div>
-            </a>
-            <a className="option_button" href="/#">
-              <div className="option_div" id="characters">
-                <img className="option_icon" src={characterIcon} alt="Characters" />
-                <p className="option_label">Characters</p>
-              </div>
-            </a>
-            <a className="option_button" href="/#">
-              <div className="option_div" id="contacts">
-                <img className="option_icon" src={folderIcon} alt="User Info" />
-                <p className="option_label">Contacts</p>
-              </div>
-            </a>
-            <a className="option_button" href="/#">
-              <div className="option_div" id="order">
-                <img className="option_icon" src={documentIcon} alt="User Info" />
-                <p className="option_label">Order</p>
-              </div>
-            </a>
-          </div>
-
+        <div id="feature_divider">
+          <p id="features_label">FEATURES</p>
         </div>
+
+        <div id="customer_options">
+          <a className="option_button" href="/#">
+            <div className="option_div" id="user_info">
+              <img className="option_icon" src={userInfoIcon} alt="User Info" onLoad={() => setHiddenImage1(false)} />
+              <p className="option_label">User Info</p>
+            </div>
+          </a>
+          <a className="option_button" href="/#">
+            <div className="option_div" id="characters">
+              <img className="option_icon" src={characterIcon} alt="Characters" onLoad={() => setHiddenImage2(false)} />
+              <p className="option_label">Characters</p>
+            </div>
+          </a>
+          <a className="option_button" href="/#">
+            <div className="option_div" id="contacts">
+              <img className="option_icon" src={folderIcon} alt="User Info" onLoad={() => setHiddenImage3(false)} />
+              <p className="option_label">Contacts</p>
+            </div>
+          </a>
+          <a className="option_button" href="/#">
+            <div className="option_div" id="order">
+              <img className="option_icon" src={documentIcon} alt="User Info" onLoad={() => setHiddenImage4(false)} />
+              <p className="option_label">Order</p>
+            </div>
+          </a>
+        </div>
+
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default CustomerHome;
